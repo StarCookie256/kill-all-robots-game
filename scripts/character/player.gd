81347527ad35dffaa41a2a4cbf4a6b532fb94b4b
Marlad <flosty90@gmail.com>
2025-12-10 20:45:54 +0700

    обнова

--- scripts/character/player.gd
+++ scripts/character/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 const SPEED = 300 # Скорость движения
 var direction = 0 # 0 - стоп, 1 - вправо, -1 - влево
+const JUMP_VELOCITY = 400
 
 func _physics_process(delta):
 	# Получаем ввод
@@ -12,6 +13,12 @@ func _physics_process(delta):
 		direction += 1
 	if Input.is_action_pressed("move_left"): # Предполагается, что "move_left" привязана к A или стрелке влево
 		direction -= 1
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		velocity.y -= JUMP_VELOCITY
 
 	# Применяем движение
 	velocity.x = direction * SPEED
