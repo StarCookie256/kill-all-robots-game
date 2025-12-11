0e24bf2af42bfc5c4d8c870b8e5a9f06e5d90f4a
LismondT <147310903+LismondT@users.noreply.github.com>
2025-12-11 17:22:29 +0700

    Сделал свой пистол рабочим, теперь он камит бензопилами

--- scripts/Weapons/Bullets/bullet_base.gd
+++ scripts/Weapons/Bullets/bullet_base.gd
@@ -1,11 +1,11 @@
 class_name BulletBase extends Area2D
 
 @export var direction: Vector2
-@export var speed: float = 100.0
+@export var speed: float = 200.0
 @export var damage: float = 1.0
 
 func _process(delta: float) -> void:
-	position += direction * speed * delta
+	global_position += direction * speed * delta
 
 func _on_area_entered(area: Area2D) -> void:
 	# код для нанесения урона
--- scripts/Weapons/weapon_base.gd
+++ scripts/Weapons/weapon_base.gd
@@ -4,8 +4,8 @@ class_name WeaponBase extends Node2D
 @export var bullet: PackedScene
 @export var bullet_spawnpoint: Node2D
 
-func shoot(direction: Vector2):
+func shoot():
 	var bullet_instance: BulletBase = bullet.instantiate()
-	bullet_instance.direction = direction
-	bullet_instance.position = bullet_spawnpoint.position
-	bullet_spawnpoint.add_child(bullet_instance)
+	bullet_instance.direction = Vector2.from_angle(global_rotation)
+	bullet_instance.global_position = bullet_spawnpoint.global_position
+	get_tree().root.get_node("Main/BulletsContainer").add_child(bullet_instance)
